a9daf8c70cfd424a01044782721172adc688be93
Mary Feofanova <mary.fifa3000@gmail.com>
2021-10-26 18:32:30 +0300

    more tests

--- task3/test_count_vectorizer.py
+++ task3/test_count_vectorizer.py
@@ -1,4 +1,5 @@
 import unittest
+
 from count_vectorizer import CountVectorizer
 
 
@@ -6,6 +7,19 @@ class TestCountVectorizer(unittest.TestCase):
 
     def assertPermutationEqual(self, first, second):
         return self.assertEqual(sorted(first), sorted(second))
+
+    @staticmethod
+    def get_col_reprs(m):
+        cols = [''] * len(m[0])
+        for i in range(len(m)):
+            for j in range(len(m[0])):
+                cols[j] += str(m[i][j])
+        return cols
+
+    def assertColPermutationEqual(self, first, second):
+        fst = self.get_col_reprs(first)
+        snd = self.get_col_reprs(second)
+        self.assertPermutationEqual(fst, snd)
 
     def test_missing_fit_transform(self):
         cv = CountVectorizer()
@@ -28,7 +42,25 @@ class TestCountVectorizer(unittest.TestCase):
         exp_m = [[1, 1, 1, 0, 0, 0, 0, 0, 0],
                  [0, 0, 0, 1, 1, 1, 0, 0, 0],
                  [0, 0, 0, 0, 0, 0, 1, 1, 1]]
+        self.assertColPermutationEqual(m, exp_m)
 
+    def test_interleaving(self):
+        cv = CountVectorizer()
+        m = cv.fit_transform(['a a a', 'a c a', 'd d a'])
+        self.assertPermutationEqual(cv.get_feature_names(), ['a', 'c', 'd'])
+        exp_m = [[3, 0, 0],
+                 [2, 1, 0],
+                 [1, 0, 2]]
+        self.assertColPermutationEqual(m, exp_m)
+
+    def test_tab_spaces(self):
+        cv = CountVectorizer()
+        m = cv.fit_transform(['a a     a', '   a\t\n c a', 'd d a\n\n\n    '])
+        self.assertPermutationEqual(cv.get_feature_names(), ['a', 'c', 'd'])
+        exp_m = [[3, 0, 0],
+                 [2, 1, 0],
+                 [1, 0, 2]]
+        self.assertColPermutationEqual(m, exp_m)
 
 
 if __name__ == '__main__':
